39ce8152eb25964a64eb2c1b513114c248deb872
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-08 14:50:49 +0530

    props pass button click usestate in functinal component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Student from "./Student";
 
 function App() {
+  const [name, setName] = useState("Anil");
   return (
     <div className="App">
       <h1>Props in React</h1>
-      <Student
-        name={"anil"}
-        email="anil@test.com"
-        other={{ address: "delhi", mobile: 111 }}
-      />
-      <Student
-        name="peter"
-        email="peter@test.com"
-        other={{ address: "noida", mobile: 222 }}
-      />
-      <Student
-        name="pri"
-        email="pri@test.com"
-        other={{ address: "gurgaon", mobile: 333 }}
-      />
+      <Student name={name} />
+      <button
+        onClick={() => {
+          setName("Sidhu");
+        }}
+      >
+        Update Name
+      </button>
     </div>
   );
 }
--- src/Student.js
+++ src/Student.js
@@ -3,9 +3,6 @@ function Student(props) {
   return (
     <div style={{ backgroundColor: "skyblue", margin: 10 }}>
       <h1>Hello {props.name}</h1>
-      <h2>Email: {props.email}</h2>
-      <h4>Address : {props.other.address}</h4>
-      <h4>Contact : {props.other.mobile}</h4>
     </div>
   );
 }
