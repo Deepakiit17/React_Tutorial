9de244fc072a558e41eae4997ba056e47031af34
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-08 15:20:17 +0530

    props in class component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 import Student from "./Student";
 
 function App() {
-  const [name, setName] = useState("Anil");
   return (
     <div className="App">
-      <h1>Props in React</h1>
-      <Student name={name} />
-      <button
-        onClick={() => {
-          setName("Sidhu");
-        }}
-      >
-        Update Name
-      </button>
+      <h1>Props in Class Component</h1>
+      <Student name="Anil" email="anil@test.com" />
     </div>
   );
 }
