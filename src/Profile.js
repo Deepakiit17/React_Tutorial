ad8d2d59ae6449169a61b6d544b0ec70e641140a
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-09 16:15:29 +0530

    Conditional Rendering 1, 2, 3

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,21 @@
 import { useState } from "react";
 
 function Profile() {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(1);
 
-  return <div>{loggedIn ? <h1>Welcome Anil</h1> : <h1>Welcome Guest</h1>}</div>;
+  //   1,2,3
+
+  return (
+    <div>
+      {loggedIn == 1 ? (
+        <h1>Welcome User 1</h1>
+      ) : loggedIn == 2 ? (
+        <h1>Welcome User 2</h1>
+      ) : (
+        <h1>Welcome User 3</h1>
+      )}
+    </div>
+  );
 }
 
 export default Profile;
