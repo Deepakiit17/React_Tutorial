c9a62cb13862d06f38e732a37b3e03e31fc8e800
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-09 14:53:52 +0530

    Hide & Show Single Button Toggle

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
   return (
     <div className="App">
       {status ? <h1>Hello World !</h1> : null}
-      <button onClick={() => setStatus(false)}>Hide</button>
-      <button onClick={() => setStatus(true)}>Show</button>
+      {/* <button onClick={() => setStatus(false)}>Hide</button> */}
+      {/* <button onClick={() => setStatus(true)}>Show</button> */}
+      <button onClick={() => setStatus(!status)}>Toggle</button>
     </div>
   );
 }
