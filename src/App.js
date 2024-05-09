bc8c40cef1d1cc7db9db5a16718e5be2cdb47cda
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-09 09:23:46 +0530

    update name button click props in class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React from "react";
 import "./App.css";
 import Student from "./Student";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Props in Class Component</h1>
-      <Student name="Anil" email="anil@test.com" />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Anil",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Props in Class Component</h1>
+        <Student name={this.state.name} email="anil@test.com" />
+        <button onClick={() => this.setState({ name: "Sidhu" })}>
+          Update Name
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
