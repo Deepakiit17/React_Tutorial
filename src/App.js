50ffe32960fadb1e7c03ddd2836642fd0e330a7d
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-09 14:49:57 +0530

    Hide & Show

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
-import Student from "./Student";
 
 function App() {
-  const [data, setData] = useState(null);
-  const [print, setPrint] = useState(false);
+  const [status, setStatus] = useState(true);
 
-  function getData(val) {
-    console.warn(val.target.value);
-    setData(val.target.value);
-    setPrint(false);
-  }
   return (
     <div className="App">
-      {print ? <h1>{data}</h1> : null}
-      <h1>Get Input box value</h1>
-      <input type="text" onChange={getData} />
-      <button onClick={() => setPrint(true)}>Print Data</button>
+      {status ? <h1>Hello World !</h1> : null}
+      <button onClick={() => setStatus(false)}>Hide</button>
+      <button onClick={() => setStatus(true)}>Show</button>
     </div>
   );
 }
