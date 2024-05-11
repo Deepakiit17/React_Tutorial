daad19bebce2c8a48137c9227765781c05c083ab
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-11 15:45:14 +0530

    componentDidMount life cycle method

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
-import User1 from "./Components/User1";
 
-function App() {
-  const [name, setName] = useState("Anil");
-
-  return (
-    <div className="App">
-      <h1>Render Method in React</h1>
-      <User1
-      //  name={name}
-      />
-      {/* <button onClick={() => setName("Sidhu")}>Update Name</button> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "anil",
+    };
+  }
+  componentDidMount() {
+    console.warn("componentDidMount");
+  }
+  render() {
+    console.warn("render");
+    return (
+      <div className="App">
+        <h1>componentDidMount in React {this.state.name}</h1>
+        <button onClick={() => this.setState({ name: "Sidhu" })}>
+          Update Name
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
