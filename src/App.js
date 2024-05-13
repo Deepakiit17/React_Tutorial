cc03c5620dd4429002347b4d28e352d38089be6f
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-13 08:16:11 +0530

    shouldComponentUpdate life cycle method

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
-    console.warn("constructor");
     this.state = {
       count: 0,
     };
   }
-  componentDidUpdate(preProps, preState, snapshot) {
-    console.warn("componentDidUpdate", preState.count, this.state.count);
-    if (preState.count === this.state.count) {
-      alert("data is already same");
-    }
+  shouldComponentUpdate() {
+    console.warn("shouldComponentUpdate", this.state.count);
+    return true
   }
   render() {
-    // console.warn("render");
     return (
       <div className="App">
-        <h1>componentDidUpdate in React {this.state.count}</h1>
-        <button onClick={() => this.setState({ count: 1 })}>Update Name</button>
+        <h1>shouldComponentUpdate in React {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Update Counter
+        </button>
       </div>
     );
   }
