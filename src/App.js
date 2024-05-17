9044c407d6725c1adc8aba81c041d935da433c69
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-17 08:25:32 +0530

    List with Bootstrap Table

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   const students = [
     { name: "anil", email: "anil@test.com", contact: 111 },
     { name: "peter", email: "peter@test.com", contact: 222 },
-    { name: "sam", email: "sam@test.com", contact: 333 },
-    { name: "sid", email: "sid@test.com", contact: 444 },
+    { name: "sam", email: "sam@test.com", contact: 111 },
+    { name: "sid", email: "sid@test.com", contact: 111 },
   ];
   return (
     <div className="App">
@@ -19,13 +19,15 @@ function App() {
             <td>Email</td>
             <td>Contact</td>
           </tr>
-          {students.map((data, i) => (
-            <tr key={i}>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.contact}</td>
-            </tr>
-          ))}
+          {students.map((data, i) =>
+            data.contact === 111 ? (
+              <tr key={i}>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.contact}</td>
+              </tr>
+            ) : null
+          )}
         </tbody>
       </Table>
     </div>
