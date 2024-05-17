79cf3323c24c418b53b839fcb3063c1c2d7b7be8
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-17 08:52:29 +0530

    Reuse Component with List

--- src/App.js
+++ src/App.js
@@ -1,82 +1,36 @@
 import "./App.css";
 import React from "react";
-import { Table } from "react-bootstrap";
+import User from "./User";
 function App() {
   const users = [
     {
       name: "Anil",
       email: "anil@test.com",
-      address: [
-        { hm: "101", city: "Noida", country: "India" },
-        { hm: "10", city: "Gurgaon", country: "India" },
-        { hm: "23", city: "Noida", country: "India" },
-        { hm: "45", city: "Delhi", country: "India" },
-      ],
+      contact: "111",
     },
     {
       name: "Burce",
       email: "bruce@test.com",
-      address: [
-        { hm: "101", city: "Noida", country: "India" },
-        { hm: "10", city: "Gurgaon", country: "India" },
-        { hm: "23", city: "Noida", country: "India" },
-        { hm: "45", city: "Delhi", country: "India" },
-      ],
+      contact: "222",
     },
     {
       name: "Peter",
       email: "peter@test.com",
-      address: [
-        { hm: "101", city: "Noida", country: "India" },
-        { hm: "10", city: "Gurgaon", country: "India" },
-        { hm: "23", city: "Noida", country: "India" },
-        { hm: "45", city: "Delhi", country: "India" },
-      ],
+      contact: "333",
     },
     {
       name: "Sam",
       email: "sam@test.com",
-      address: [
-        { hm: "101", city: "Noida", country: "India" },
-        { hm: "10", city: "Gurgaon", country: "India" },
-        { hm: "23", city: "Noida", country: "India" },
-        { hm: "45", city: "Delhi", country: "India" },
-      ],
+      contact: "444",
     },
   ];
   return (
     <div className="App">
-      <h1>List with Nested Array</h1>
-      <Table variant="dark">
-        <tbody>
-          <tr>
-            <td>S.N</td>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Address</td>
-          </tr>
-          {users.map((item, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              <td>
-                <Table variant="dark">
-                  <tbody>
-                    {item.address.map((data) => (
-                      <tr>
-                        <td>{data.hm}</td>
-                        <td>{data.city}</td>
-                        <td>{data.country}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <h1>Reuse Component with List</h1>
+
+      {users.map((item) => (
+        <User data={item} />
+      ))}
     </div>
   );
 }
