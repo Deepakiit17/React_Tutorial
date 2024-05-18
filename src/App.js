a734682df1aa839e3e12c2b463c1cbd4f9057341
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-18 14:55:23 +0530

    Uncontrolled Component in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
-  let [val, setVal] = useState("111");
+  let inputRef = useRef(null);
+  let inputRef2 = useRef(null);
+
+  function submitForm(e) {
+    e.preventDefault();
+    console.warn("input field 1 value : ", inputRef.current.value);
+    console.warn("input field 2 value : ", inputRef2.current.value);
+    let input3 = document.getElementById("input3").value;
+    console.warn("input field 2 value : ", input3);
+  }
   return (
     <div className="App">
-      <h1>Controlled Component in React </h1>
-      <input type="text" value={val} onChange={(e) => setVal(e.target.value)} />
-      <h3>value {val}</h3>
+      <h1>Uncontrolled Component in React </h1>
+      <form onSubmit={submitForm}>
+        <input type="text" ref={inputRef} /> <br /> <br />
+        <input type="text" ref={inputRef2} /> <br /> <br />
+        <input type="text" id="input3" /> <br /> <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
