695022ee6bb693afb9190647064396bcf600f491
Deepak Meena <deepak07itmeena@gmail.com>
2024-05-19 08:32:32 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
-      <h1>HOC </h1>
-      <HOCRed cmp={Counter} />
-      <HOCGreen cmp={Counter} />
-      <HOCBlue cmp={Counter} />
-    </div>
-  );
-}
-function HOCRed(props) {
-  return (
-    <h2 style={{ backgroundColor: "red", width: 100 }}>
-      Red
-      <props.cmp />
-    </h2>
-  );
-}
-function HOCGreen(props) {
-  return (
-    <h2 style={{ backgroundColor: "green", width: 100 }}>
-      Grren
-      <props.cmp />
-    </h2>
-  );
-}
-function HOCBlue(props) {
-  return (
-    <h2 style={{ backgroundColor: "blue", width: 100 }}>
-      blue <props.cmp />
-    </h2>
-  );
-}
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>Update</button>
+      <Nav />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
